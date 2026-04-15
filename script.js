8933b4e8fb50db6141964b3f4d650ed6371dd17c
Your Name <cnipt69@gmail.com>
2026-04-15 14:53:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+// let names = ["Ibrokhim", "Nikhola", "Samir", "Timur", "Doniyor", "Sitora"]
+
+// let callback = (name) => name.length % 2 !== 0
+
+// let a = names.filter( callback )
+
+// let b = names.find( callback )
+
+// let c = []
+// names.forEach ( (name) => {
+//     if (name.length % 2 !== 0){
+//         c.push(name)
+//     }
+// })
+
+
+// Отфильтровать задачи в переменные а и b через filter или через foreach
+    // Сохранить количество в ключе count
+    // Сохранить сами задачи в массиве-ключе arr
+let arr = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut au`tem",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 4,
+        "title": "et porro tempora",
+        "completed": true
+    },
+    {
+        "userId": 1,
+        "id": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 6,
+        "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 7,
+        "title": "illo expedita consequatur quia in",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 8,
+        "title": "quo adipisci enim quam ut ab",
+        "completed": true
+    },
+    {
+        "userId": 1,
+        "id": 9,
+        "title": "molestiae perspiciatis ipsa",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 10,
+        "title": "illo est ratione doloremque quia maiores aut",
+        "completed": true
+    },
+]
+
+let a = {
+    count: 0,
+    arr: []
+}
+
+let b = {
+    count: 0,
+    arr: []
+}
+
+arr.forEach(task => {
+    if (task.completed) {
+        a.arr.push(task);
+        a.count++;
+    } else {
+        b.arr.push(task);
+        b.count++;
+    }
+});
+
+console.log('выполнено (a):', a);
+console.log('невыполнено (b):', b);
